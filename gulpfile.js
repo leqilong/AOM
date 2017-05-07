44fc8e21e2e879aed743938a88eeb08bcf8d4611
Myles Chatman <myles@imgix.com>
2017-05-07 11:19:01 -0700

    Add jscs and jshint

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,48 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserSync = require('browser-sync'),
     browserify = require('browserify'),
-    source = require('vinyl-source-stream');
-    buffer = require('gulp-buffer');
+    source = require('vinyl-source-stream'),
+    buffer = require('gulp-buffer'),
+    jscs = require('gulp-jscs'),
+    jshint = require('gulp-jshint');
 //     uglify = require('gulp-uglify'),
 //     image_min = require('gulp-imagemin'),
 //     livereload = require('gulp-livereload'),
-//     jshint = require('gulp-jshint'),
 //     del = require('del');
-//
+
 gulp.task('default', function(){
   return gutil.log('Gulp is running!');
-})
+});
+
+gulp.task('style', function() {
+  return gulp.src([
+    '**/*.js',
+    '!node_modules{},/**}',
+    '!build{},/**}'
+  ])
+  .pipe(jscs())
+  .pipe(jshint())
+  .pipe(jshint.reporter());
+});
 
 gulp.task('phaser', function(){
   gulp.src('./node_modules/phaser/build/phaser.min.js')
-    .pipe(gulp.dest('./build/scripts'))
-})
+    .pipe(gulp.dest('./build/scripts'));
+});
 
 gulp.task('serve', function(){
   browserSync.init({
     server: {
       baseDir: './build'
     }
-  })
-  gulp.watch('./src/**/*.js').on('change', browserSync.reload)
-
-})
+  });
+  gulp.watch('./src/**/*.js').on('change', browserSync.reload);
+});
 
 gulp.task('static', function(){
   return gulp.src('./static/**/*')
-    .pipe(gulp.dest('./build'))
-})
+    .pipe(gulp.dest('./build'));
+});
 
 gulp.task('build', function() {
   browserify({
@@ -42,6 +53,6 @@ gulp.task('build', function() {
   .bundle()
   .pipe(source('game.js'))
   .pipe(gulp.dest('./build/scripts'));
-})
+});
 
-gulp.task('default', ['phaser', 'serve','static', 'build']);
+gulp.task('default', ['phaser', 'serve','static', 'build', 'style']);
