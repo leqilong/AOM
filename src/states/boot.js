b361b773e4f6a44bd541ecf0e16c554e85f5dbff
Leqi Long <leqilong@Leqis-MacBook-Pro-2.local>
2017-05-08 18:36:34 -0700

    Added test tiles and hero

--- src/states/boot.js
+++ src/states/boot.js
@@ -2,8 +2,8 @@
 
 exports.preload = function (game) {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-}
+};
 
 exports.create = function (game) {
   game.state.start('preload');
-}
+};
--- src/states/preload.js
+++ src/states/preload.js
@@ -1,9 +1,40 @@
 'use strict';
 
+function Hero(game, x, y) {
+    // call Phaser.Sprite constructor
+  Phaser.Sprite.call(this, game, x, y, 'hero');
+  this.anchor.set(0.5,0.5);
+}
+
+// inherit from Phaser.Sprite
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
+
 exports.preload = function (game) {
-  this.game.load.image('jaime', 'assets/jaime.png');
+  game.load.json('level:0', 'data/level0.json');
+  game.load.image('tile', 'assets/tiles/tile.png');
+  game.load.image('hero', 'assets/goat.png');
 };
 
 exports.create = function (game) {
-  this.game.add.image(0, 0, 'jaime');
+  game.add.image(0, 0, 'jaime');
+  this._loadLevel(game.cache.getJSON('level:0'));
+};
+
+exports._loadLevel = function (data) {
+  // spawn all platforms
+  data.platforms.forEach(this._spawnPlatform, this);
+  //spawn heroes and enemies
+  this._spawnCharacters({hero: data.hero});
+};
+
+exports._spawnPlatform = function (platform) {
+  this.game.add.sprite(platform.x, platform.y, platform.image);
+};
+
+exports._spawnCharacters = function (data){
+  //spawn hero
+  this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+  this.game.add.existing(this.hero);
 };
